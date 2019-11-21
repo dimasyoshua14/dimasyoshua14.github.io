64a07f86b7c9b0f2d47447824e74a76ca7ea0a11
= <=>
2019-11-21 09:40:46 +0700

    UGD PWA Mantap

---
+++ precache-manifest.05c678fb4bab0b173b1d420da6fc3e0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c9500e7a6ba7d371e5c",
+    "url": "/css/about.d443fafb.css"
+  },
+  {
+    "revision": "585fa5e9c2b78366880b",
+    "url": "/css/chunk-vendors.c893da24.css"
+  },
+  {
+    "revision": "40b54400f75bc9e0ee83b354aba020b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c9500e7a6ba7d371e5c",
+    "url": "/js/about.6fbed563.js"
+  },
+  {
+    "revision": "15187ae080ae7247b705",
+    "url": "/js/app.12ac9695.js"
+  },
+  {
+    "revision": "585fa5e9c2b78366880b",
+    "url": "/js/chunk-vendors.1467ee69.js"
+  },
+  {
+    "revision": "3d4da85576aef8ca87ac270008021b21",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
